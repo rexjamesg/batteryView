21d0929bb5458a834e1eef8f86fbb583c1e2f172
林於豊 <rexjamesg@gmail.com>
2019-11-29 16:41:33 +0800

    Initial Commit

---
+++ TestBatteryView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBatteryView
+//
+//  Created by Yu Li Lin on 2019/11/29.
+//  Copyright © 2019 Yu Li Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

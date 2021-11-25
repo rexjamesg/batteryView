07332d5b59fe7b2f249e9ff36951b2ea0ef85974
“rexjamesg” <rexjamesg@gmail.comgit config --global user.email rexjamesg@gmail.com>
2021-11-25 12:13:18 +0800

    優化程式碼

--- TestBatteryView/BatteryView.swift
+++ TestBatteryView/BatteryView.swift
@@ -12,11 +12,22 @@ import UIKit
 
 class BatteryView: UIView {
 
+    //電池外框線粗細
     private let lineWidth:CGFloat = 1.5
+    //框線與間距之間的留白(為了美觀)
     private let innerPadding:CGFloat = 1.5
+    //電池頭部上緣內縮距離
+    private let headSpace:CGFloat = 3.5
+    
+    //外框路徑
+    let path = UIBezierPath()
+    //外框塗層
+    let shapeLayer = CAShapeLayer()
+    
+    //電量著色路徑
     private let fillPath = UIBezierPath()
+    //電量著色圖層
     private let fillShapeLayer = CAShapeLayer()
-    private let space:CGFloat = 2.5
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -36,19 +47,34 @@ class BatteryView: UIView {
     private func setUp() {
         backgroundColor = .clear
         
-        let path = UIBezierPath()
-        path.move(to: CGPoint.init(x: 0, y: 0))
-        path.addLine(to: CGPoint.init(x: frame.size.width-space, y: 0))
-        path.addLine(to: CGPoint.init(x: frame.size.width-space, y: frame.size.height*0.2))
-        path.addLine(to: CGPoint.init(x: frame.size.width, y: frame.size.height*0.2))
-        path.addLine(to: CGPoint.init(x: frame.size.width, y: frame.size.height*0.8))
-        path.addLine(to: CGPoint.init(x: frame.size.width-space, y: frame.size.height*0.8))
-        path.addLine(to: CGPoint.init(x: frame.size.width-space, y: frame.size.height))
-        path.addLine(to: CGPoint.init(x: 0, y: frame.size.height))
-        path.close()
+        let points = setOutterLinePoints()
+        drawOutterLine(points: points)
+    }
+    //MARK: - 外框
+    //設定外框路徑
+    private func setOutterLinePoints() -> [CGPoint] {
+        return [
+            CGPoint.init(x: frame.size.width-headSpace, y: 0),
+            CGPoint.init(x: frame.size.width-headSpace, y: frame.size.height*0.2),
+            CGPoint.init(x: frame.size.width, y: frame.size.height*0.2),
+            CGPoint.init(x: frame.size.width, y: frame.size.height*0.8),
+            CGPoint.init(x: frame.size.width-headSpace, y: frame.size.height*0.8),
+            CGPoint.init(x: frame.size.width-headSpace, y: frame.size.height),
+            CGPoint.init(x: 0, y: frame.size.height)
+        ]
+    }
+    
+    //繪製外框
+    private func drawOutterLine(points:[CGPoint]) {
         
-        //design path in layer
-        let shapeLayer = CAShapeLayer()
+        path.move(to: CGPoint.init(x: 0, y: 0))
+        
+        for point in points {
+            path.addLine(to: point)
+        }
+        
+        path.close()
+                
         shapeLayer.path = path.cgPath
         shapeLayer.strokeColor = UIColor.lightGray.cgColor
         shapeLayer.lineWidth = lineWidth
@@ -56,45 +82,79 @@ class BatteryView: UIView {
         layer.addSublayer(shapeLayer)
     }
     
-    func setBatteryLevel(batteryLevel:CGFloat) {
-        fillPath.removeAllPoints()
-        fillShapeLayer.removeFromSuperlayer()
+    //MARK: - 內部著色
+    //設定著色路徑
+    private func setFillPathPoints(startX:CGFloat) -> [CGPoint] {
         
-        if batteryLevel <= 0 {
-            return
-        }
+        var points:[CGPoint] = []
         
-        let startX = frame.size.width*batteryLevel
         var shouldDrawHead:Bool = false
+        
         if startX >= frame.size.width-2.5 {
             shouldDrawHead = true
         }
                 
         if shouldDrawHead {
             //電池凸起的頭部
-            fillPath.move(to: CGPoint(x: frame.size.width-space-innerPadding, y: frame.size.height*0.8-innerPadding))
-            fillPath.addLine(to: CGPoint(x: frame.size.width-innerPadding, y: frame.size.height*0.8-innerPadding))
-            fillPath.addLine(to: CGPoint(x: frame.size.width-innerPadding, y: frame.size.height*0.2+innerPadding))
-            fillPath.addLine(to: CGPoint(x: frame.size.width-space-innerPadding, y: frame.size.height*0.2+innerPadding))
-            fillPath.addLine(to: CGPoint(x: frame.size.width-space-innerPadding, y: 0+innerPadding))
+            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: frame.size.height*0.8-innerPadding))
+            points.append(CGPoint(x: frame.size.width-innerPadding, y: frame.size.height*0.8-innerPadding))
+            points.append(CGPoint(x: frame.size.width-innerPadding, y: frame.size.height*0.2+innerPadding))
+            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: frame.size.height*0.2+innerPadding))
+            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: 0+innerPadding))
         } else {
-            fillPath.move(to: CGPoint(x: startX-innerPadding, y: 0+innerPadding))
+            points.append(CGPoint(x: startX-innerPadding, y: 0+innerPadding))
         }
         
-        fillPath.addLine(to: CGPoint(x: 0+innerPadding, y: 0+innerPadding))
-        fillPath.addLine(to: CGPoint(x: 0+innerPadding, y: frame.size.height-innerPadding))
+        points.append(CGPoint(x: 0+innerPadding, y: 0+innerPadding))
+        points.append(CGPoint(x: 0+innerPadding, y: frame.size.height-innerPadding))
         
         if shouldDrawHead {
             //電池凸起的頭部
-            fillPath.addLine(to: CGPoint(x: frame.size.width-space-innerPadding, y: frame.size.height-innerPadding))
+            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: frame.size.height-innerPadding))
         } else {
-            fillPath.addLine(to: CGPoint(x: startX-innerPadding, y: frame.size.height-innerPadding))
+            points.append(CGPoint(x: startX-innerPadding, y: frame.size.height-innerPadding))
         }
-
+        
+        return points
+    }
+    
+    //繪製路徑並著色
+    private func drawInnerLine(points:[CGPoint]) {
+                
+        //每次繪製時，清掉所有路徑，避免重複疊加
+        fillPath.removeAllPoints()
+        fillShapeLayer.removeFromSuperlayer()
+        
+        for (index, point) in points.enumerated() {
+            if index == 0 {
+                fillPath.move(to: point)
+            } else {
+                fillPath.addLine(to: point)
+            }
+        }
+        
+        fillPath.close()
         fillShapeLayer.path = fillPath.cgPath
         fillShapeLayer.strokeColor = UIColor.clear.cgColor
         fillShapeLayer.lineWidth = lineWidth
+    }
+    
+    /**
+     依照電量動態填滿顏色
+     - Parameter batteryLevel: 輸入的電量
+     */
+    func setBatteryLevel(batteryLevel:CGFloat) {
         
+        if batteryLevel <= 0 {
+            return
+        }
+        
+        let startX = frame.size.width*batteryLevel
+        
+        let points = setFillPathPoints(startX: startX)
+        
+        drawInnerLine(points: points)
+                                       
         var color = UIColor.green.cgColor
         
         if batteryLevel > 0.2 && batteryLevel < 0.5 {

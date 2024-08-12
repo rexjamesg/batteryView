424d39414a10d62dd1107d938a045610a1424c70
林於豊 <rexjamesg@gmail.com>
2024-08-12 18:16:56 +0800

    1. 修正低電量時，電量圖層超出框線問題。 2. 整理程式碼

--- TestBatteryView/BatteryView.swift
+++ TestBatteryView/BatteryView.swift
@@ -10,121 +10,159 @@ import UIKit
 
 import UIKit
 
-class BatteryView: UIView {
+// MARK: - BatteryView
 
-    //電池外框線粗細
-    private let lineWidth:CGFloat = 1.5
-    //框線與間距之間的留白(為了美觀)
-    private let innerPadding:CGFloat = 1.5
-    //電池頭部上緣內縮距離
-    private let headSpace:CGFloat = 3.5
+class BatteryView: UIView {
     
-    //外框路徑
-    let path = UIBezierPath()
-    //外框塗層
-    let shapeLayer = CAShapeLayer()
-    
-    //電量著色路徑
+    //MARK: - Private Properties
+    // 外框路徑
+    private let path = UIBezierPath()
+    // 外框塗層
+    private let shapeLayer = CAShapeLayer()
+        
+    // 電池外框線粗細
+    private let lineWidth: CGFloat = 1.5
+    // 框線與間距之間的留白(為了美觀)
+    private let innerPadding: CGFloat = 1.5
+    // 電池頭部上緣內縮距離
+    private let headSpace: CGFloat = 3.5
+    // 電量著色路徑
     private let fillPath = UIBezierPath()
-    //電量著色圖層
+    // 電量著色圖層
     private let fillShapeLayer = CAShapeLayer()
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setUp()
     }
-    
+
     override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         setUp()
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUp()
     }
-    
-    private func setUp() {
-        backgroundColor = .clear
+
+    /**
+     依照電量動態填滿顏色
+     - Parameter batteryLevel: 輸入的電量
+     */
+    func setBatteryLevel(batteryLevel: CGFloat) {
+        let startX = frame.size.width * batteryLevel
+        //避免電量顯示圖層低於左方外框線範圍
+        if startX <= lineWidth+innerPadding {
+            return
+        }
         
-        let points = setOutterLinePoints()
+        let points = setFillPathPoints(startX: startX)
+        drawInnerLine(points: points)
+        var color = UIColor.green.cgColor
+        if batteryLevel > 0.2 && batteryLevel < 0.5 {
+            color = UIColor.yellow.cgColor
+        } else if batteryLevel <= 0.2 {
+            color = UIColor.red.cgColor
+        }
+
+        fillShapeLayer.fillColor = color
+        layer.addSublayer(fillShapeLayer)
+    }
+
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+         // Drawing code
+     }
+     */
+}
+
+// MARK: - Private Methods
+
+private extension BatteryView {
+    func setUp() {
+        backgroundColor = .clear
+        let points = getOutterLinePoints()
         drawOutterLine(points: points)
     }
-    //MARK: - 外框
-    //設定外框路徑
-    private func setOutterLinePoints() -> [CGPoint] {
+
+    // MARK: - 外框
+
+    // 設定電池外框路徑
+    func getOutterLinePoints() -> [CGPoint] {
         return [
-            CGPoint.init(x: frame.size.width-headSpace, y: 0),
-            CGPoint.init(x: frame.size.width-headSpace, y: frame.size.height*0.2),
-            CGPoint.init(x: frame.size.width, y: frame.size.height*0.2),
-            CGPoint.init(x: frame.size.width, y: frame.size.height*0.8),
-            CGPoint.init(x: frame.size.width-headSpace, y: frame.size.height*0.8),
-            CGPoint.init(x: frame.size.width-headSpace, y: frame.size.height),
-            CGPoint.init(x: 0, y: frame.size.height)
+            CGPoint(x: frame.size.width - headSpace, y: 0),
+            CGPoint(x: frame.size.width - headSpace, y: frame.size.height * 0.2),
+            CGPoint(x: frame.size.width, y: frame.size.height * 0.2),
+            CGPoint(x: frame.size.width, y: frame.size.height * 0.8),
+            CGPoint(x: frame.size.width - headSpace, y: frame.size.height * 0.8),
+            CGPoint(x: frame.size.width - headSpace, y: frame.size.height),
+            CGPoint(x: 0, y: frame.size.height),
         ]
     }
-    
-    //繪製外框
-    private func drawOutterLine(points:[CGPoint]) {
-        
-        path.move(to: CGPoint.init(x: 0, y: 0))
-        
+
+    // 繪製外框
+    func drawOutterLine(points: [CGPoint]) {
+        path.move(to: CGPoint(x: 0, y: 0))
+
         for point in points {
             path.addLine(to: point)
         }
-        
+
         path.close()
-                
+
         shapeLayer.path = path.cgPath
         shapeLayer.strokeColor = UIColor.lightGray.cgColor
         shapeLayer.lineWidth = lineWidth
         shapeLayer.fillColor = UIColor.clear.cgColor
         layer.addSublayer(shapeLayer)
     }
-    
-    //MARK: - 內部著色
-    //設定著色路徑
-    private func setFillPathPoints(startX:CGFloat) -> [CGPoint] {
-        
-        var points:[CGPoint] = []
-        
-        var shouldDrawHead:Bool = false
-        
-        if startX >= frame.size.width-2.5 {
+
+    // MARK: - 內部著色
+
+    // 設定著色路徑
+    func setFillPathPoints(startX: CGFloat) -> [CGPoint] {
+        var points: [CGPoint] = []
+        var shouldDrawHead = false
+
+        if startX >= frame.size.width - 2.5 {
             shouldDrawHead = true
+        } else if startX <= 0.01 {
+            return [CGPoint.zero]
         }
-                
+
         if shouldDrawHead {
-            //電池凸起的頭部
-            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: frame.size.height*0.8-innerPadding))
-            points.append(CGPoint(x: frame.size.width-innerPadding, y: frame.size.height*0.8-innerPadding))
-            points.append(CGPoint(x: frame.size.width-innerPadding, y: frame.size.height*0.2+innerPadding))
-            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: frame.size.height*0.2+innerPadding))
-            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: 0+innerPadding))
+            // 電池凸起的頭部
+            points.append(CGPoint(x: frame.size.width - headSpace - innerPadding, y: frame.size.height * 0.8 - innerPadding))
+            points.append(CGPoint(x: frame.size.width - innerPadding, y: frame.size.height * 0.8 - innerPadding))
+            points.append(CGPoint(x: frame.size.width - innerPadding, y: frame.size.height * 0.2 + innerPadding))
+            points.append(CGPoint(x: frame.size.width - headSpace - innerPadding, y: frame.size.height * 0.2 + innerPadding))
+            points.append(CGPoint(x: frame.size.width - headSpace - innerPadding, y: 0 + innerPadding))
         } else {
-            points.append(CGPoint(x: startX-innerPadding, y: 0+innerPadding))
+            points.append(CGPoint(x: startX - innerPadding, y: 0 + innerPadding))
         }
-        
-        points.append(CGPoint(x: 0+innerPadding, y: 0+innerPadding))
-        points.append(CGPoint(x: 0+innerPadding, y: frame.size.height-innerPadding))
-        
+
+        points.append(CGPoint(x: 0 + innerPadding, y: 0 + innerPadding))
+        points.append(CGPoint(x: 0 + innerPadding, y: frame.size.height - innerPadding))
+
         if shouldDrawHead {
-            //電池凸起的頭部
-            points.append(CGPoint(x: frame.size.width-headSpace-innerPadding, y: frame.size.height-innerPadding))
+            // 電池凸起的頭部
+            points.append(CGPoint(x: frame.size.width - headSpace - innerPadding, y: frame.size.height - innerPadding))
         } else {
-            points.append(CGPoint(x: startX-innerPadding, y: frame.size.height-innerPadding))
+            points.append(CGPoint(x: startX - innerPadding, y: frame.size.height - innerPadding))
         }
-        
+
         return points
     }
-    
-    //繪製路徑並著色
-    private func drawInnerLine(points:[CGPoint]) {
-                
-        //每次繪製時，清掉所有路徑，避免重複疊加
+
+    // 繪製路徑並著色
+    func drawInnerLine(points: [CGPoint]) {
+        // 每次繪製時，清掉所有路徑，避免重複疊加
         fillPath.removeAllPoints()
         fillShapeLayer.removeFromSuperlayer()
-        
+
         for (index, point) in points.enumerated() {
             if index == 0 {
                 fillPath.move(to: point)
@@ -132,47 +170,10 @@ class BatteryView: UIView {
                 fillPath.addLine(to: point)
             }
         }
-        
+
         fillPath.close()
         fillShapeLayer.path = fillPath.cgPath
         fillShapeLayer.strokeColor = UIColor.clear.cgColor
         fillShapeLayer.lineWidth = lineWidth
     }
-    
-    /**
-     依照電量動態填滿顏色
-     - Parameter batteryLevel: 輸入的電量
-     */
-    func setBatteryLevel(batteryLevel:CGFloat) {
-        
-        if batteryLevel <= 0 {
-            return
-        }
-        
-        let startX = frame.size.width*batteryLevel
-        
-        let points = setFillPathPoints(startX: startX)
-        
-        drawInnerLine(points: points)
-                                       
-        var color = UIColor.green.cgColor
-        
-        if batteryLevel > 0.2 && batteryLevel < 0.5 {
-            color = UIColor.yellow.cgColor
-        } else if batteryLevel <= 0.2 {
-            color = UIColor.red.cgColor
-        }
-        
-        fillShapeLayer.fillColor = color
-        layer.addSublayer(fillShapeLayer)
-    }
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
--- TestBatteryView/ViewController.swift
+++ TestBatteryView/ViewController.swift
@@ -8,46 +8,55 @@
 
 import UIKit
 
+// MARK: - ViewController
+
 class ViewController: UIViewController {
-    var batteryView:BatteryView = BatteryView()
-    var sliderLabel:UILabel = UILabel()
-    
+    //MARK: - Private Properties
+    private var batteryView: BatteryView = .init()
+    private var slider = UISlider()
+    private var sliderLabel: UILabel = .init()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-                
-        batteryView = BatteryView.init(frame: CGRect.init(x: 0, y: 0, width: 50, height: 20))
-        batteryView.center = view.center
-        view.addSubview(batteryView)
-        
-        
-        let slider = UISlider.init(frame: CGRect.init(x: 0, y: batteryView.frame.origin.y+100, width: view.frame.size.width*0.5, height: 50))
+
+        setBatterView()
+        setSlider()
+        setSliderLabel(slider: slider)
+        batteryView.setBatteryLevel(batteryLevel: CGFloat(slider.value))
+    }
+}
+
+// MARK: - Private Methods
+
+private extension ViewController {
+    func setSlider() {
+        slider = UISlider(frame: CGRect(x: 0, y: batteryView.frame.origin.y + 100, width: view.frame.size.width * 0.5, height: 50))
         slider.center.x = view.center.x
         view.addSubview(slider)
         slider.minimumValue = 0
         slider.maximumValue = 1
         slider.addTarget(self, action: #selector(didChangeSliderValue(sender:)), for: .valueChanged)
         slider.value = 0.5
-        
-        
-        sliderLabel = UILabel.init(frame: CGRect.init(x: 0, y: batteryView.frame.origin.y+50, width: 100, height: 40))
+    }
+    
+    @objc func didChangeSliderValue(sender: UISlider) {
+        batteryView.setBatteryLevel(batteryLevel: CGFloat(sender.value))
+        sliderLabel.text = "\(Int(sender.value * 100))%"
+    }
+
+    func setBatterView() {
+        batteryView = BatteryView(frame: CGRect(x: 0, y: 0, width: 50, height: 20))
+        batteryView.center = view.center
+        view.addSubview(batteryView)
+    }
+
+    func setSliderLabel(slider: UISlider) {
+        sliderLabel = UILabel(frame: CGRect(x: 0, y: batteryView.frame.origin.y + 50, width: 100, height: 40))
         sliderLabel.textColor = UIColor.black
         sliderLabel.center.x = view.center.x
-        sliderLabel.text = "\(Int(slider.value*100))%"
+        sliderLabel.text = "\(Int(slider.value * 100))%"
         sliderLabel.textAlignment = .center
         view.addSubview(sliderLabel)
-        
-        
-        batteryView.setBatteryLevel(batteryLevel: CGFloat(slider.value))
     }
-    
-    @objc func didChangeSliderValue(sender:UISlider) {
-        
-        batteryView.setBatteryLevel(batteryLevel: CGFloat(sender.value))
-        
-        sliderLabel.text = "\(Int(sender.value*100))%"
-    }
-    
 }
-
-
